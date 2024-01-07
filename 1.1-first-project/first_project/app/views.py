3c5a21cdb24f43bca1c14177be34514444868df3
yakovlevasp <yakovleva.s.p@yandex.ru>
2024-01-07 14:58:56 +0300

    homework 1.1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,3 +1,6 @@
+"""
+Django view functions
+"""
 import datetime
 import os
 
@@ -6,13 +9,16 @@ from django.shortcuts import render, reverse
 
 
 def home_view(request):
+    """
+    View-функция главной страницы
+    """
     template_name = 'app/home.html'
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -22,7 +28,10 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    """
+    View-функция страницы, показывающей текущую дату и время
+    """
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
     current_time = datetime.datetime.now()
     msg = f'Текущее время: {current_time.strftime("%d.%m.%Y %H:%M:%S")}'
@@ -30,5 +39,8 @@ def time_view(request):
 
 
 def workdir_view(request):
+    """
+    View-функция страницы с содержимым рабочей директории
+    """
     dir_content = '</p><p>'.join(os.listdir())
     return HttpResponse(f'<h1>Cписок файлов:</h1><p>{dir_content}</p>')
