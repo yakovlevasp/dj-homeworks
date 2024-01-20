aac59ace2a337b4f6d752358b89223ccc89f6aee
yakovlevasp <yakovleva.s.p@yandex.ru>
2024-01-21 00:12:27 +0300

    homework 1.2 pagination

--- 1.2-requests-templates/pagination/pagination/settings.py
+++ 1.2-requests-templates/pagination/pagination/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
 
 BUS_STATION_CSV = os.path.join(BASE_DIR, 'data-398-2018-08-30.csv')
 
+BUS_STATION_PAGE_SIZE = 10
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,33 @@
+"""
+Django view functions
+"""
+import csv
+
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+from django.core.paginator import Paginator
 
 
 def index(request):
+    """
+    View-функция главной страницы. Перенаправление на список остановок
+    """
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
+    """
+    View-функция страницы со списком остановок
+    """
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
+    context = {}
+    with open(settings.BUS_STATION_CSV, encoding='utf-8') as csvfile:
+        paginator = Paginator(
+            list(csv.DictReader(csvfile)),
+            settings.BUS_STATION_PAGE_SIZE
+        )
+        context['page'] = paginator.get_page(request.GET.get('page', 1))
+        context['bus_stations'] = context['page'].object_list
     return render(request, 'stations/index.html', context)
