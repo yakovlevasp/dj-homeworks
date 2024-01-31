92015781bc776216d9ae96b94e3c225518df75f9
yakovlevasp <yakovleva.s.p@yandex.ru>
2024-01-31 18:33:11 +0300

    homework 2.2 orm_migrations

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,15 +1,21 @@
-from django.views.generic import ListView
+"""
+View-функции
+"""
 from django.shortcuts import render
 
 from .models import Student
 
 
 def students_list(request):
+    """
+    View-функция списка учеников
+    """
     template = 'school/students_list.html'
-    context = {}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-
+    context = {
+        'object_list': Student.objects.order_by(ordering).prefetch_related('teacher')
+    }
     return render(request, template, context)
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -21,6 +21,8 @@ SECRET_KEY = 'b0e@^m&tccz11$w59qov$lhn-97!(%wfn-gray-c*x)^a$wx=2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+INTERNAL_IPS = ['127.0.0.1']
+
 ALLOWED_HOSTS = []
 
 
@@ -36,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'school',
 ]
 
+STATIC_URL = '/static/'
+
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,6 +82,8 @@ DATABASES = {
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': os.getenv('PG_USER'),
+        'PASSWORD': os.getenv('PG_PASSWORD')
     }
 }
 
