03443cf2779c0f8fb80b1d0b3feb1f0ee0b47283
yakovlevasp <yakovleva.s.p@yandex.ru>
2024-02-06 17:47:25 +0300

    homework 3.1 smart_home

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,22 @@
+"""
+Модели
+"""
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    """
+    Модель датчика
+    """
+    name = models.TextField()
+    description = models.TextField()
+
+
+class Measurement(models.Model):
+    """
+    Модель измерения
+    """
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='measurement/img', null=True)
--- 3.1-drf-intro/smart_home/smart_home/settings.py
+++ 3.1-drf-intro/smart_home/smart_home/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .settings_local import *
+except ImportError:
+    pass
